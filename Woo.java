da209d42d0e6b70f563ad26a96771e3cf585c7a7
George Liang <gliang90@stuy.edu>
2018-01-11 23:37:45 -0500

    Added methods to Woo.java

--- Woo.java
+++ Woo.java
@@ -1,6 +1,39 @@
 import java.util.ArrayList;
 
 public class Woo{
+    private Card[] _deck; // Container of cards from which players draw from 
+    private Card[] _discardPile; // Container of cards already played 
+    private Card[] _topcard; // Container for card 
+    private Player[] _turnOrder; // _turnOrder
+    private Player _currentPlayer; 
+
+    // Default constructor Woo() instantiates  _deck 
+    public Woo(){
+    }
+ 
+    public void setup(){
+    }
+
+    public void playerDraw(Player p){
+    }
+    
+    public boolean anyWinner(){
+	return false;
+    }
+    
+    public void shuffle(){
+    }
+
+    public void distribute(){
+    }
+    
+    public void playCard(Player p, int i){
+    }
+    
+    public static boolean isPlayable(){
+	return false;
+    }
+    
     public static void main(String[]args){
     }
 }
