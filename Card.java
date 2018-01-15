cbd89803ea66b99100e72b052ba1c685afa7ade9
ACJMR <chosking@stuy.edu>
2018-01-15 00:42:32 -0500

    added more methods to Card

--- Card.java
+++ Card.java
@@ -2,14 +2,21 @@ public abstract class Card{
     private int _type; 
     private int _suite;
     
+    
     public int getType(){
-	return _type;
+	   return _type;
     }
 
     public int getSuite(){
-	return _suite;
+	   return _suite;
     }
 
+    public boolean isPlayable(){ //determines whther the card is playable according to the _topCard
+        
+    }
     
+    public void action(){ //We
+        
+    }
     
 }
